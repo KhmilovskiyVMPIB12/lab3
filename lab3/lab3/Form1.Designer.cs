6b3c9497a0b6cc5bbd40a48d38f757e6f8f54e17
KhmilovskiyVMPIB12 <125645185+KhmilovskiyVMPIB12@users.noreply.github.com>
2023-06-02 18:51:53 +0700

    Add files via upload

---
+++ lab3/lab3/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnHello = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(15, 60);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(145, 20);
+            this.tbName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Как зовут?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnHello
+            // 
+            this.btnHello.Location = new System.Drawing.Point(15, 95);
+            this.btnHello.Name = "btnHello";
+            this.btnHello.Size = new System.Drawing.Size(101, 23);
+            this.btnHello.TabIndex = 2;
+            this.btnHello.Text = "Таки здресте";
+            this.btnHello.UseVisualStyleBackColor = true;
+            this.btnHello.Click += new System.EventHandler(this.btnHello_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 124);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Выйти";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(232, 197);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnHello);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnHello;
+        private System.Windows.Forms.Button btnExit;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ lab3/lab3/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle; // Запретить изменение размера формы
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHello_Click(object sender, EventArgs e)
+        {
+            //получаем значение текстового поля + отрезаем пробелы по краям;
+            string userName = tbName.Text.Trim();
+            if (userName.Length < 3)
+            {
+                MessageBox.Show("введите имя пользователя!");
+            }
+            else
+            {
+                MessageBox.Show("Привет " + userName);
+            }
+        }
+    }
+}
